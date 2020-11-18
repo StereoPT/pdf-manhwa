cbe6119257fcaf184cbcbab779bc0c4607c66913
GuidoSP <stereopt@gmail.com>
2020-11-18 23:48:08 +0000

    Chapter Parts on PDF

--- parsers/mangazuki.js
+++ parsers/mangazuki.js
@@ -1,14 +1,19 @@
+const fs = require('fs');
 const readLine = require('readline-sync');
 const axios = require('axios');
 const path = require('path');
 const { downloadImage } = require('../lib/imageHelper');
+const PDFDocument = require('pdfkit');
+const wait = require('waait');
 
 module.exports = {
   name: 'Mangazuki',
   host: 'mangazuki.me',
   url: 'https://mangazuki.me/',
   async parse(cheerio, html, chapterUrl) {
+    const chapterDoc = new PDFDocument({ autoFirstPage: false });
     let imagePromises = [];
+    let imagesPath = [];
 
     const $ = cheerio.load(html);
     const mangaTitle = $('meta[name="twitter:title"]').attr('content').split(' ').join('_');
@@ -18,15 +23,23 @@ module.exports = {
     imageObject.each(async function(index, img) {
       const imgSrc = img.attribs.src;
       const imgPadding = img.attribs['data-image-paged'];
-      const imgName = mangaTitle + "-" + imgPadding + ".jpg";
+      const imgName = mangaTitle + "-" + imgPadding + ".png";
       const imgPath = path.join(__dirname, '..', 'images', imgName);
 
+      imagesPath.push(imgPath);
       imagePromises.push(downloadImage(imgSrc, imgPath));
     });
 
     Promise.all(imagePromises).then(async () => {
-      let searchNextPage = nextPage.split('?')[0];
+      chapterDoc.pipe(fs.createWriteStream(path.join(__dirname, '..', `${chapterUrl.split('/').pop()}.pdf`)));
+      for(let image of imagesPath) {
+        let pdfImage = chapterDoc.openImage(image);
+        chapterDoc.addPage({ size: [ pdfImage.width, pdfImage.height ] });
+        chapterDoc.image(pdfImage, 0, 0);
+      }
+      chapterDoc.end();
 
+      let searchNextPage = nextPage.split('?')[0];
       if(chapterUrl.search(searchNextPage) == -1) {
         const nextChapter = readLine.question('Download Next Chapter [Y/N]: ').toUpperCase();
         if(nextChapter == 'Y') {
